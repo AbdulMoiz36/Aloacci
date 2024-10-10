38c607c9d42536d1cef88999abd1be72f1ad738f
Sajjad <sajjadsaleem341@gmail.com>
2024-10-10 13:55:28 +0500

    updated database

--- db/aloacci.sql
+++ db/aloacci.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2024 at 02:41 PM
+-- Generation Time: Oct 10, 2024 at 10:54 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin_role`
+--
+
+CREATE TABLE `admin_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_role`
+--
+
+INSERT INTO `admin_role` (`id`, `role`) VALUES
+(1, 'Admin'),
+(2, 'Employee');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `admin_user`
 --
 
@@ -32,7 +51,7 @@ CREATE TABLE `admin_user` (
   `image` varchar(255) NOT NULL,
   `name` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `role` varchar(50) NOT NULL,
+  `role_id` int(10) NOT NULL,
   `email` varchar(50) NOT NULL,
   `mobile` varchar(50) NOT NULL,
   `status` int(11) NOT NULL
@@ -42,10 +61,12 @@ CREATE TABLE `admin_user` (
 -- Dumping data for table `admin_user`
 --
 
-INSERT INTO `admin_user` (`id`, `image`, `name`, `password`, `role`, `email`, `mobile`, `status`) VALUES
-(10, '20220228_204126.jpg', 'Sajjad', 'sajjad125', 'admin', 'sajjadsaleem341@gmail.com', '03176122252', 1),
-(12, '20190814_184724.jpg', 'Sahil', 'sahil', 'employee', 'sahil@gmail.com', '03242477248', 1),
-(18, 'istockphoto-947269088-612x612.jpg', 'Sameer', 'sameer', 'employee', 'sameer@gmail.com', '03352350927', 1);
+INSERT INTO `admin_user` (`id`, `image`, `name`, `password`, `role_id`, `email`, `mobile`, `status`) VALUES
+(10, '20220228_204126.jpg', 'Sajjad', 'sajjad125', 1, 'sajjadsaleem341@gmail.com', '03176122252', 1),
+(12, '20190814_184724.jpg', 'Sahil', 'sahil', 2, 'sahil@gmail.com', '03242477248', 1),
+(18, 'istockphoto-947269088-612x612.jpg', 'Sameer', 'sameer', 2, 'sameer@gmail.com', '03352350927', 1),
+(19, 'download (1).png', 'Moiz', 'moiz', 1, 'moiz@gmail.com', '03245633589', 1),
+(20, 'download (1).png', 'Ahmer', 'ahmer', 2, 'ahmer@gmail.com', '03452488598', 0);
 
 -- --------------------------------------------------------
 
@@ -57,23 +78,6 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL,
   `categories` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `categories`) VALUES
-(1, 'Gender'),
-(2, 'Range'),
-(3, 'Weekly Deals'),
-(4, 'Best Sellers'),
-(5, 'Perfume Wax'),
-(6, 'Body Mist'),
-(7, 'Genre'),
-(8, 'Type'),
-(9, 'Sample Set'),
-(10, 'Air Freshners'),
-(11, 'Elite Fragrances');
 
 -- --------------------------------------------------------
 
@@ -89,14 +93,6 @@ CREATE TABLE `contact_us` (
   `message` text NOT NULL,
   `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `contact_us`
---
-
-INSERT INTO `contact_us` (`id`, `name`, `email`, `subject`, `message`, `date`) VALUES
-(2, 'Abid', 'abid211@gmail.com', 'Subject', 'Message', '2022-11-02 20:52:48'),
-(40, 'Sajjad', 'sajjadsaleem341@gmail.com', 'Test', 'Lorem', '2024-08-24 11:32:58');
 
 -- --------------------------------------------------------
 
@@ -165,24 +161,18 @@ INSERT INTO `lasting` (`id`, `lasting`) VALUES
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `tracking_id` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `mobile` varchar(15) NOT NULL,
   `address` varchar(255) NOT NULL,
   `city` varchar(50) NOT NULL,
+  `shipping` int(11) NOT NULL,
   `total_price` float NOT NULL,
+  `order_from` tinyint(4) NOT NULL,
   `order_status` int(11) NOT NULL,
   `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `tracking_id`, `user_id`, `email`, `mobile`, `address`, `city`, `total_price`, `order_status`, `date`) VALUES
-(1, '#2041746', 22, 'sajjadsaleem341@gmail.com', '03112656651', 'H#194, St#01, Scheme 33', 'Karachi', 5200, 2, '2024-10-02'),
-(2, '#2424901', 1, 'kashif321@gmail.com', '32165498700', 'H#194, St#01', 'Karachi', 4700, 1, '2024-10-02');
 
 -- --------------------------------------------------------
 
@@ -194,20 +184,10 @@ CREATE TABLE `orders_detail` (
   `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
-  `format` int(11) NOT NULL,
+  `format` varchar(10) NOT NULL,
   `qty` int(11) NOT NULL,
   `price` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `orders_detail`
---
-
-INSERT INTO `orders_detail` (`id`, `order_id`, `product_id`, `format`, `qty`, `price`) VALUES
-(1, 1, 3, 50, 2, 1400),
-(2, 1, 2, 50, 3, 800),
-(3, 2, 2, 100, 2, 1500),
-(4, 2, 1, 50, 1, 1700);
 
 -- --------------------------------------------------------
 
@@ -243,8 +223,8 @@ CREATE TABLE `product` (
   `sub_category_id` int(11) NOT NULL,
   `image` varchar(255) NOT NULL,
   `image2` varchar(255) NOT NULL,
+  `image3` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `qty` int(11) NOT NULL,
   `gender_id` int(11) NOT NULL,
   `genre_id` int(11) NOT NULL,
   `type_id` int(11) NOT NULL,
@@ -255,15 +235,6 @@ CREATE TABLE `product` (
   `breif` varchar(2000) NOT NULL,
   `status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`id`, `category_id`, `sub_category_id`, `image`, `image2`, `name`, `qty`, `gender_id`, `genre_id`, `type_id`, `season_id`, `sillage_id`, `lasting_id`, `description`, `breif`, `status`) VALUES
-(1, 1, 1, 'lostsymbol.jpeg', 'lostsymbol_40_11zon.jpeg', 'Musk', 50, 1, 2, 2, 1, 3, 3, 'Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.', 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.', 1),
-(2, 1, 2, 'lostsymbol_40_11zon.jpeg', 'lostsymbol.jpeg', 'Musk e Fakhir', 100, 3, 2, 1, 2, 1, 2, 'Reference site about Lorem Ipsum, giving information on its origins', 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.', 1),
-(3, 2, 3, 'lostsymbol.jpeg', '', 'Blue Sea', 25, 2, 1, 2, 1, 2, 1, 'Organizations recognize the value of analytics.', '', 1);
 
 -- --------------------------------------------------------
 
@@ -279,18 +250,6 @@ CREATE TABLE `product_format` (
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `product_format`
---
-
-INSERT INTO `product_format` (`id`, `product_id`, `format`, `price`, `qty`) VALUES
-(17, 1, '50', 1700, 0),
-(18, 1, '100', 3200, 0),
-(19, 2, '50', 800, 0),
-(20, 2, '100', 1500, 0),
-(21, 3, '12', 350, 0),
-(22, 3, '50', 1400, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -301,8 +260,11 @@ CREATE TABLE `reviews` (
   `id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
+  `format` varchar(10) NOT NULL,
   `rating` int(11) NOT NULL,
-  `comment` text NOT NULL
+  `comment` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -359,21 +321,6 @@ CREATE TABLE `sub_categories` (
   `status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `sub_categories`
---
-
-INSERT INTO `sub_categories` (`id`, `category_id`, `sub_categories`, `status`) VALUES
-(1, 1, 'Men', 1),
-(2, 1, 'Women', 1),
-(3, 2, 'Explorer', 1),
-(4, 2, 'Executive', 1),
-(5, 2, 'Elite', 1),
-(6, 3, 'Inferno', 1),
-(7, 3, 'Lady Eve', 1),
-(8, 3, 'Dauntless', 1),
-(13, 3, 'Saiful Malook', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -415,15 +362,17 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `mobile`, `city`, `address`, `date`) VALUES
-(1, 'Kashif', 'kashif321@gmail.com', 'kashif321', '32165498700', '', '', '2022-11-01 00:18:40'),
-(22, 'Sajjad', 'sajjadsaleem341@gmail.com', 'sajjad125', '03112656651', '', '', '2022-12-01 01:36:29'),
-(23, 'Salman', 'salman321@gmail.com', 'salman321', '65498732112', '', '', '2022-12-02 01:09:58'),
-(24, 'Amir', 'amir@gmail.com', 'amir', '03115625891', '', '', '2024-09-14 04:04:02'),
-(25, 'Moiz', 'moiz@gmail.com', 'moiz', '02136589511', '', '', '2024-09-14 04:16:21');
+(1, 'Sajjad', 'sajjadsaleem341@gmail.com', 'sajjad', '03176122252', '', '', '2024-10-10 09:45:05');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin_role`
+--
+ALTER TABLE `admin_role`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `admin_user`
@@ -539,22 +488,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `admin_role`
+--
+ALTER TABLE `admin_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `admin_user`
 --
 ALTER TABLE `admin_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `contact_us`
 --
 ALTER TABLE `contact_us`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `gender`
@@ -578,13 +533,13 @@ ALTER TABLE `lasting`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `orders_detail`
 --
 ALTER TABLE `orders_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `order_status`
@@ -596,13 +551,13 @@ ALTER TABLE `order_status`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `product_format`
 --
 ALTER TABLE `product_format`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `reviews`
@@ -626,7 +581,7 @@ ALTER TABLE `sillage`
 -- AUTO_INCREMENT for table `sub_categories`
 --
 ALTER TABLE `sub_categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `type`
@@ -638,7 +593,7 @@ ALTER TABLE `type`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
