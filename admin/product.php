059b9ed57e4d0b9773f070714366377dc8f9fa2d
Sajjad <sajjadsaleem341@gmail.com>
2024-10-19 19:50:29 +0500

    id replace with serial no.

--- admin/product.php
+++ admin/product.php
@@ -31,7 +31,7 @@ $select = "
 
 $res = mysqli_query($con, $select);
 
-
+$serial_no = 1;
 ?>
 <div class="row">
    <div class="col-12">
@@ -45,7 +45,7 @@ $res = mysqli_query($con, $select);
                <table class="table table-striped" id="table-1">
                   <thead>
                      <tr>
-                        <th>ID</th>
+                        <th>Serial No.</th>
                         <th>Categories</th>
                         <th>P_Name</th>
                         <th>Image</th>
@@ -60,7 +60,7 @@ $res = mysqli_query($con, $select);
                   <tbody>
                      <?php while($row = mysqli_fetch_array($res)){ ?>
                      <tr>
-                        <td> <?= $row['id'] ?></td>
+                        <td> <?= $serial_no++; ?> </td>
                         <td> <?= $row['categories'] ?> </td>
                         <td> <?= $row['name'] ?> </td>
                         <td>
