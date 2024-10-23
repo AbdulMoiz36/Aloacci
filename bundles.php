1d831cd29714cc454c21da2f57a97720f7c54191
AbdulMoiz36 <abdulmoiz8236@gmail.com>
2024-10-24 02:05:30 +0500

    Bundles Design Complete

---
+++ bundles.php
@@ -0,0 +1,78 @@
+<?php
+include "header.php";
+?>
+
+<section class="w-full flex flex-col items-center justify-center py-10">
+   <div>
+        <h1 class="text-3xl lg:text-5xl font-bold text-center">Bundles</h1>
+    </div>
+    <!-- Single Bundle Start -->
+    <div class="flex flex-col w-3/4 lg:w-2/4 mx-auto my-14">
+        <h1 class="text-lg md:text-xl lg:text-2xl font-extrabold text-center my-2 ">Add Bundles To Cart And Get 5% Off!</h1>
+        <!-- Images -->
+        <div class="flex justify-center">
+            <div class="w-2/4 border-2">
+                <img src="./image/Crossmyheartcopy_13_11zon.jpeg" alt="">
+                <div class="py-5 pl-3">
+                    <a href="product_details.php?id="><p class="font-bold underline md:text-lg">Product Name</p></a>
+                    <p>Format</p>
+                    <p>Qty:1</p>
+                    <p class="font-bold">Rs.1701 <span class="line-through text-sm">Rs.1790</span></p>
+                </div>
+            </div>
+            <p class="self-center bg-black text-white rounded-full w-6 h-6 text-center pt-1 pl-0.5 text-xs"><i class="fa-solid fa-plus"></i></p>
+           <div class="w-2/4 border-2">
+               <img src="./image/TippingPoint.jpeg" alt="">
+               <div class="py-5 pl-3">
+                    <a href="product_details.php?id="><p class="font-bold underline md:text-lg">Product Name</p></a>
+                    <p>Format</p>
+                    <p>Qty:1</p>
+                    <p class="font-bold">Rs.1701 <span class="line-through text-sm">Rs.1790</span></p>
+                </div>
+           </div>
+        </div>
+        <!-- Price And Details -->
+        <div class="flex flex-col justify-center mt-5 gap-4">
+            <p class="text-center">Total Price: Rs.2926 <span class="line-through">Rs.3000</span> <span class="font-bold">Save Rs.154</span></p>
+            <div onclick="" class="w-full p-3 text-center border-2 hover:cursor-pointer bg-gradient-to-bl from-yellow-500 via-yellow-500 to-amber-600 shadow-sm hover:shadow-xl transition-shadow ease-in-out duration-300 font-semibold rounded-full text-white">Buy It Now</div>
+        </div>
+    </div>
+    <!-- Single Bundle End -->
+    <!-- Single Bundle Start -->
+    <div class="flex flex-col w-3/4 lg:w-2/4 mx-auto my-14">
+        <h1 class="text-lg md:text-xl lg:text-2xl font-extrabold text-center my-2 ">Add Bundles To Cart And Get 5% Off!</h1>
+        <!-- Images -->
+        <div class="flex justify-center">
+            <div class="w-2/4 border-2">
+                <img src="./image/Crossmyheartcopy_13_11zon.jpeg" alt="">
+                <div class="py-5 pl-3">
+                    <a href="product_details.php?id="><p class="font-bold underline md:text-lg">Product Name</p></a>
+                    <p>Format</p>
+                    <p>Qty:1</p>
+                    <p class="font-bold">Rs.1701 <span class="line-through text-sm">Rs.1790</span></p>
+                </div>
+            </div>
+            <p class="self-center bg-black text-white rounded-full w-6 h-6 text-center pt-1 pl-0.5 text-xs"><i class="fa-solid fa-plus"></i></p>
+           <div class="w-2/4 border-2">
+               <img src="./image/TippingPoint.jpeg" alt="">
+               <div class="py-5 pl-3">
+                    <a href="product_details.php?id="><p class="font-bold underline md:text-lg">Product Name</p></a>
+                    <p>Format</p>
+                    <p>Qty:1</p>
+                    <p class="font-bold">Rs.1701 <span class="line-through text-sm">Rs.1790</span></p>
+                </div>
+           </div>
+        </div>
+        <!-- Price And Details -->
+        <div class="flex flex-col justify-center mt-5 gap-4">
+            <p class="text-center">Total Price: Rs.2926 <span class="line-through">Rs.3000</span> <span class="font-bold">Save Rs.154</span></p>
+            <div onclick="" class="w-full p-3 text-center border-2 hover:cursor-pointer bg-gradient-to-bl from-yellow-500 via-yellow-500 to-amber-600 shadow-sm hover:shadow-xl transition-shadow ease-in-out duration-300 font-semibold rounded-full text-white">Buy It Now</div>
+        </div>
+    </div>
+    <!-- Single Bundle End -->
+    
+</section>
+
+<?php
+include "footer.php";
+?>
